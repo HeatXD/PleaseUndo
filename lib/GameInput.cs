46e24c582945b5565c55c31e84f8a7059b88c273
HeatXD <45072324+HeatXD@users.noreply.github.com>
2021-11-26 23:27:19 +0100

    implementation of gameinput almost done need to look into logging tho

--- lib/GameInput.cs
+++ lib/GameInput.cs
@@ -1,31 +1,54 @@
-using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace PleaseUndo
 {
     struct GameInput<T>
     {
         const int GAMEINPUT_MAX_PLAYERS = 2;
+
+        T[] inputs;
+        int frame;
+
         enum Constants
         {
             NullFrame = -1
         }
-        T[] inputs = new T[GAMEINPUT_MAX_PLAYERS];
-        int frame;
-        bool IsNull() => frame == Constants.NullFrame;
-        void Init(int frame, T input, int offset)
-        {
 
-        }
-        void Init(int frame, T[] inputs)
+        void Init(int frame, T[] game_inputs, int offset)
         {
+            this.inputs = new T[GAMEINPUT_MAX_PLAYERS];
             this.frame = frame;
         }
-        bool Value(int i) => input[i] != null;
+
+        void Init(int frame, T[] game_inputs)
+        {
+            Trace.Assert(game_inputs.Length <= this.inputs.Length);
+            this.inputs = game_inputs;
+            this.frame = frame;
+        }
+
+        bool Equal(GameInput<T> game_input, bool inputs_only)
+        {
+            if (!inputs_only && frame != game_input.frame)
+            {
+                Log("frames don't match");
+            }
+            if (Enumerable.SequenceEqual(inputs, game_input.inputs))
+            {
+                Log("inputs don't match\n");
+            }
+            return (inputs_only ||
+             game_input.frame == frame &&
+             Enumerable.SequenceEqual(inputs, game_input.inputs));
+        }
+
+        void Erase() => inputs = new T[GAMEINPUT_MAX_PLAYERS];
+        bool IsNull() => frame == (int)Constants.NullFrame;
+        bool Value(int i) => inputs[i] != null;
         void Set(int i, T input) => inputs[i] = input;
-        void Clear(int i) => inputs[i] = null;
-        void Erase() => Array.forEach(inputs, input = null);
-        void Desc();
-        void Log();
-        bool Equal();
+        void Clear(int i) => inputs[i] = default(T);
+        void Desc() => System.Console.WriteLine("TODO");
+        void Log(string msg) => System.Console.WriteLine(msg);
     }
 }
