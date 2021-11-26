dabe0af3b971ff5033cf9a21c844dcd21ead7b1c
JonathanPicques <jonathan.picques@gmail.com>
2021-11-26 22:29:37 +0100

    Add PleaseUndo lib csproj

--- test/Scripts/TestScript.cs
+++ test/Scripts/TestScript.cs
@@ -3,19 +3,9 @@ using System;
 
 public class TestScript : Node
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("OK!");
-	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Ready()
+    {
+        GD.Print("OK!");
+        PleaseUndo.GameInput<int> gameInput;
+    }
 }
