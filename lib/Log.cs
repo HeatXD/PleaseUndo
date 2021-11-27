73bab61ff99d01480aea3844617ba87a9c3906b2
HeatXD <45072324+HeatXD@users.noreply.github.com>
2021-11-27 13:03:31 +0100

    loggin

--- lib/Log.cs
+++ lib/Log.cs
@@ -1,10 +1,19 @@
+using System.IO;
+
 namespace PleaseUndo
 {
     public class Logger
     {
+        static bool firstLog = true;
         public static void Log(string fmt, params object[] args)
         {
-            System.Console.WriteLine(string.Format(fmt, args));
+
+            LogToFile(string.Format(fmt, args));
+        }
+
+        public static void LogToFile(string message)
+        {
+
         }
     }
 }
