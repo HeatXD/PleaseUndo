18188a5c33568485cb8eb927cc7c76a0fdf56599
HeatXD <45072324+HeatXD@users.noreply.github.com>
2021-11-27 13:33:24 +0100

    logging to file and to console

--- lib/Log.cs
+++ lib/Log.cs
@@ -4,16 +4,34 @@ namespace PleaseUndo
 {
     public class Logger
     {
+        const string logPath = "pu_log_info.txt";
+        static bool logToFile = true;
+
         static bool firstLog = true;
+
         public static void Log(string fmt, params object[] args)
         {
-
-            LogToFile(string.Format(fmt, args));
+            if (logToFile)
+            {
+                LogToFile(string.Format(fmt, args));
+            }
+            else
+            {
+                System.Console.WriteLine(string.Format(fmt, args));
+            }
         }
 
         public static void LogToFile(string message)
         {
-
+            if (firstLog)
+            {
+                File.WriteAllText(logPath, message);
+                firstLog = false;
+            }
+            else
+            {
+                File.AppendAllText(logPath, message);
+            }
         }
 
         public static void Assert(bool condition)
--- test/Scripts/TestScript.cs
+++ test/Scripts/TestScript.cs
@@ -3,9 +3,11 @@ using PleaseUndo;
 
 public class TestScript : Node
 {
-	public override void _Ready()
-	{
-		GameInput<int> input = new GameInput<int>();
-		input.Init(10, new int[] { 1, 3 });
-	}
+    public override void _Ready()
+    {
+        GameInput<int> input = new GameInput<int>();
+        input.Init(10, new int[] { 1, 3 });
+        input.Log("Loggin", true);
+        input.Log("Loggin", true);
+    }
 }
