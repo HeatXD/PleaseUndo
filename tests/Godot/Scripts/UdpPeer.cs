fe4aa02881bd02b813bfd547e83c7c5d9f681b80
HeatXD <45072324+HeatXD@users.noreply.github.com>
2021-12-27 15:42:56 +0100

    better example

--- tests/Godot/Scripts/UdpPeer.cs
+++ tests/Godot/Scripts/UdpPeer.cs
@@ -6,12 +6,10 @@ using System.Collections.Generic;
 public class UdpPeer<InputType> : IPeerNetAdapter<InputType>
 {
     private PacketPeerUDP _peer;
-    private List<NetMsg> _last_messages;
 
     public Error Init(int localPort, String remoteAddress, int remotePort)
     {
         _peer = new PacketPeerUDP();
-        _last_messages = new List<NetMsg>();
 
         var destErr = _peer.SetDestAddress(remoteAddress, remotePort);
         var listenErr = _peer.Listen(localPort);
@@ -22,15 +20,14 @@ public class UdpPeer<InputType> : IPeerNetAdapter<InputType>
         return Error.Ok;
     }
 
-
     private void SendMsg(byte[] msg)
     {
         _peer.PutPacket(msg);
     }
 
-    // Show be called every iteration
-    public void Poll()
+    public List<NetMsg> Poll()
     {
+        var messages = new List<NetMsg>();
         if (_peer.IsListening())
         {
             if (_peer.GetAvailablePacketCount() > 0)
@@ -38,10 +35,11 @@ public class UdpPeer<InputType> : IPeerNetAdapter<InputType>
                 for (int i = 0; i < _peer.GetAvailablePacketCount(); i++)
                 {
                     var msg = _peer.GetPacket();
-                    _last_messages.Add(NetMsg.Deserialize<NetMsg>(msg));
+                    messages.Add(NetMsg.Deserialize<NetMsg>(msg));
                 }
             }
         }
+        return messages;
     }
 
     public override void Send(NetMsg msg)
@@ -51,9 +49,7 @@ public class UdpPeer<InputType> : IPeerNetAdapter<InputType>
 
     public override List<NetMsg> ReceiveAllMessages()
     {
-        var temp = new List<NetMsg>(_last_messages);
-        _last_messages.Clear();
-        return temp;
+        return Poll();
     }
 }
 
