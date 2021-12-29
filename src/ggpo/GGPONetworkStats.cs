e7b8f663f752896676bf4ea96ad7fcef2ad9ad0e
JonathanPicques <jonathan.picques@gmail.com>
2021-12-29 19:07:22 +0100

    Forget a namespace

--- src/ggpo/GGPONetworkStats.cs
+++ src/ggpo/GGPONetworkStats.cs
@@ -1,18 +1,21 @@
-public struct GGPONetworkStats
+namespace PleaseUndo
 {
-    public struct _Network
+    public struct GGPONetworkStats
     {
-        public int send_queue_len;
-        public int recv_queue_len;
-        public int ping;
-        public int kbps_sent;
-    }
-    public struct _Timesync
-    {
-        public int local_frames_behind;
-        public int remote_frames_behind;
-    }
+        public struct _Network
+        {
+            public int send_queue_len;
+            public int recv_queue_len;
+            public int ping;
+            public int kbps_sent;
+        }
+        public struct _Timesync
+        {
+            public int local_frames_behind;
+            public int remote_frames_behind;
+        }
 
-    public _Network Network;
-    public _Timesync Timesync;
+        public _Network Network;
+        public _Timesync Timesync;
+    }
 }
