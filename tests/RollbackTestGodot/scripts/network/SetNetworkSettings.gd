1ba7d5c16776fcfa823907e189482f2a1a2c2596
JonathanPicques <jonathan.picques@gmail.com>
2021-12-31 18:34:03 +0100

    Add Quick P1/P2 setup

--- tests/RollbackTestGodot/scripts/network/SetNetworkSettings.gd
+++ tests/RollbackTestGodot/scripts/network/SetNetworkSettings.gd
@@ -22,3 +22,17 @@ func _on_BTN_START_button_up():
 		get_tree().change_scene("res://scenes/Gameplay.tscn")
 	else:
 		OS.alert("Please fill in all the required information!")
+
+func _on_BTN_1P_pressed():
+	$BG/LE_LOCALPORT.text = "7001"
+	$BG/LE_REMOTEPORT.text = "7002"
+	$BG/LE_REMOTEIP.text = "127.0.0.1"
+	$BG/LE_LOCALID.text = "1"
+	$BG/LE_DELAY.text = "0"
+
+func _on_BTN_2P_pressed():
+	$BG/LE_LOCALPORT.text = "7002"
+	$BG/LE_REMOTEPORT.text = "7001"
+	$BG/LE_REMOTEIP.text = "127.0.0.1"
+	$BG/LE_LOCALID.text = "2"
+	$BG/LE_DELAY.text = "0"
